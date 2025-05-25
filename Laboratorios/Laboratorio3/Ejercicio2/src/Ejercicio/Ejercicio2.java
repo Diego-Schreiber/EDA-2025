f660771e8831cdf8f7e10494b26b87d40a6086ae
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-24 21:44:08 -0500

    Añadiendo main del ejercicio 2

---
+++ Laboratorios/Laboratorio3/Ejercicio2/src/Ejercicio/Ejercicio2.java
@@ -0,0 +1,34 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase main para el ejercicio2
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int opcion=0;
+        double rpta=0;
+        while(opcion!=8){
+            double v=0;
+            System.out.println("1.Suma\n2.Resta\n3.Producto\n4.Division\n5.Potencia\n6.Raiz Cuadrada\n7.Raiz cubica\n8.Salir");
+            System.out.print("Ingrese operacion: ");
+            opcion=scan.nextInt();
+            if(opcion>0&&opcion<6){
+                System.out.print("Ingrese dato: ");
+                v=scan.nextDouble();
+            }
+            System.out.print("Ingrese dato: ");
+            double v1=scan.nextDouble();
+            switch(opcion){
+                case 1: rpta=suma(v,v1); break;
+                case 2: rpta=resta(v,v1); break;
+                case 3: rpta=producto(v,v1); break;
+                case 4: rpta=division(v,v1); break;
+                case 5: rpta=potencia(v,v1); break;
+                case 6: rpta=raizCuadrada(v1); break;
+                case 7: rpta=raizCubica(v1); break;
+                case 8: System.out.println("Chau"); break;
+                default: System.out.println("Operacion invalida"); break;
+            }
+        }
+    }
+}

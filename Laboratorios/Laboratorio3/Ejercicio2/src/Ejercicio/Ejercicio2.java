00abe8e4406868b914e6f40b0c87d62cc879020c
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-24 21:46:59 -0500

    producto de clase ejercicio2

--- Laboratorios/Laboratorio3/Ejercicio2/src/Ejercicio/Ejercicio2.java
+++ Laboratorios/Laboratorio3/Ejercicio2/src/Ejercicio/Ejercicio2.java
@@ -37,4 +37,7 @@ public class Ejercicio2 {
     static <T extends Number>double resta(T valor1, T valor2){
         return valor1.doubleValue()-valor2.doubleValue();
     }
+    static <T extends Number>double producto(T valor1, T valor2){
+        return valor1.doubleValue()*valor2.doubleValue();
+    }
 }

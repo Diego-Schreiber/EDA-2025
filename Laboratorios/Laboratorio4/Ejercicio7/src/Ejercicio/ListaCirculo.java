c0856675591d35bb917e10907dfdfa90723148c5
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-31 23:47:46 -0500

    Clase lista circulo constructor y atributos

---
+++ Laboratorios/Laboratorio4/Ejercicio7/src/Ejercicio/ListaCirculo.java
@@ -0,0 +1,11 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase lista circular
+public class ListaCirculo<T> {
+    private Nodo<T> cabeza;
+    private Nodo<T> cola;
+    public ListaCirculo(){
+        cabeza=null;
+        cola=null;
+    }
+}

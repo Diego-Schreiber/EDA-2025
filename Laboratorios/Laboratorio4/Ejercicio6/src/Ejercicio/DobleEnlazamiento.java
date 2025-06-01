a1c1c555f45b3d5f06c0bf6c71805bfd6e6ca46c
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-31 23:40:39 -0500

    clase doble enlazamiento(constructor y atributos)

---
+++ Laboratorios/Laboratorio4/Ejercicio6/src/Ejercicio/DobleEnlazamiento.java
@@ -0,0 +1,11 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase de lista doble enlazada
+public class DobleEnlazamiento<T>{
+    private Nodo<T> cabeza;
+    private Nodo<T> cola;
+    public DobleEnlazamiento(){
+        cabeza=null;
+        cola=null;
+    }
+}

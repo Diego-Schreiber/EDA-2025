0c38cee14208b4bbdaf985fddc3aed1416282aef
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-31 23:18:29 -0500

    Main del ejercicio4

--- Laboratorios/Laboratorio4/Ejercicio4/src/Ejercicio/Ejercicio4.java
+++ Laboratorios/Laboratorio4/Ejercicio4/src/Ejercicio/Ejercicio4.java
@@ -1,23 +1,20 @@
 package Ejercicio;
 //Autor: Diego Schreiber
-//Lista doble enlazada
+//Main del ejercicio4
 import java.util.LinkedList;
-import java.util.ListIterator;
-public class Ejercicio3{
+public class Ejercicio4{
     public static void main(String[] args) {
         LinkedList<Integer> lista = new LinkedList<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 12; i++) {
             lista.add(i);
         }
-        System.out.println("Lista hacia adelante:");
-        for (Integer numero : lista) {
-            System.out.print(numero + " ");
-        }
-        System.out.println();
-        System.out.println("Lista hacia atras:");
-        ListIterator<Integer> iterador = lista.listIterator(lista.size());
-        while (iterador.hasPrevious()) {
-            System.out.print(iterador.previous() + " ");
+        System.out.println("Recorrido circular (2 ciclos):");
+        int ciclos = 2;
+        int total = lista.size() * ciclos;
+        int index = 0;
+        for (int i = 0; i < total; i++) {
+            System.out.print(lista.get(index) + " ");
+            index = (index + 1) % lista.size(); 
         }
         System.out.println();
     }

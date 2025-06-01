1d8e2d926cf33299f1d5d773209a5edb3fa182cb
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-31 22:56:02 -0500

    linkedlist constructor y atributos

---
+++ Laboratorios/Laboratorio4/Ejercicio1/src/Ejercicio/linkedList.java
@@ -0,0 +1,11 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Lista doblemente enlazada
+public class linkedList <E>{
+    private Nodo<E> cabeza;
+    private Nodo<E> cola;
+    public linkedList(){
+        cabeza=null;
+        cola=null;
+    }
+}

68292a08c2292c1ac7f946be8da323b767ad1313
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-31 23:24:08 -0500

    Lista enlazada constructor y atributos

---
+++ Laboratorios/Laboratorio4/Ejercicio5/src/Ejercicio/listaEnlazada.java
@@ -0,0 +1,9 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase lista con todos lo metodos
+public class listaEnlazada<T>{
+    private Nodo<T> cabeza;
+    public listaEnlazada(){
+        cabeza=null;
+    }
+}

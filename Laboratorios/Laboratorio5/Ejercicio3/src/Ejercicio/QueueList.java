4aea88b99ccce98690bf66ccbe8609689dc1bcd3
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-08 00:26:50 -0500

    Reutilizando nodo + CLase queueList(cosntructor y atributos)

---
+++ Laboratorios/Laboratorio5/Ejercicio3/src/Ejercicio/QueueList.java
@@ -0,0 +1,10 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase para la cola
+public class QueueList<T>{
+    private Nodo<T> frente;
+    private Nodo<T> fin;
+    public QueueList(){
+        frente = fin = null;
+    }
+}

ae166ae5e0350771c1125b6c1eb6639e1fc52b83
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-16 13:47:49 -0500

    Añadiendo estructura lab 6 y ejercicio 1

---
+++ Laboratorios/Laboratorio6/Ejercicio1/src/Ejercicio/Node.java
@@ -0,0 +1,17 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase nodo para BST
+public class Node <T extends Comparable>{
+    private T dato;
+    private Node<T> left;
+    private Node<T> right;
+    public Node(T d, Node<T> l, Node<T> r){
+        dato=d;
+        left=l;
+        right=r;
+    }
+    public Node(T d){
+        this(d,null,null);
+    }
+
+}

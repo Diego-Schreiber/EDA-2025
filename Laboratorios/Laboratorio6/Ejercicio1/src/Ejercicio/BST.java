d9c6ed491f7c3305aa87a4635ec6f3681fe041e6
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-16 13:58:00 -0500

    Añadiendo metodos para insertaar en BST

--- Laboratorios/Laboratorio6/Ejercicio1/src/Ejercicio/BST.java
+++ Laboratorios/Laboratorio6/Ejercicio1/src/Ejercicio/BST.java
@@ -18,4 +18,21 @@ public class BST <T extends Comparable> {
     public void destroy(){
         root=null;
     }
+    public void insert(T x){
+        this.root = insertNode(x, this.root);
+    }
+    protected Node<T> insertNode(T x, Node<T> actual) {
+        if (actual == null) {
+            return new Node<>(x);
+        }
+        int resC = actual.getDato().compareTo(x);
+        if (resC == 0) {
+            System.out.println(x + " esta duplicado");
+        } else if (resC < 0) {
+            actual.setRight(insertNode(x, actual.getRight()));
+        } else {
+            actual.setLeft(insertNode(x, actual.getLeft()));
+        }
+        return actual;
+    }
 }

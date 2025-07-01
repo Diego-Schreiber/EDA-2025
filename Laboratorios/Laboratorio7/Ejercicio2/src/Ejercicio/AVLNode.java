d5e3322121c29d316ecb1b08a2ccda2c1f16b22a
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-30 20:02:49 -0500

    setters y getters de AVLNode

--- Laboratorios/Laboratorio7/Ejercicio2/src/Ejercicio/AVLNode.java
+++ Laboratorios/Laboratorio7/Ejercicio2/src/Ejercicio/AVLNode.java
@@ -9,4 +9,28 @@ public class AVLNode<T extends Comparable<T>>{
         this.valor = valor;
         this.altura = 1;
     }
+    public void setValor(T v){
+        valor=v;
+    }
+    public T getValor(){
+        return valor;
+    }
+    public void setIzquierda(AVLNode i){
+        izquierda=i;
+    }
+    public AVLNode getIzquierda(){
+        return izquierda;
+    }
+    public void setDerecha(AVLNode d){
+        izquierda=d;
+    }
+    public AVLNode getDerecha(){
+        return derecha;
+    }
+    public void setAltura(int a){
+        altura=a;
+    }
+    public int getAltura(){
+        return altura;
+    }
 }

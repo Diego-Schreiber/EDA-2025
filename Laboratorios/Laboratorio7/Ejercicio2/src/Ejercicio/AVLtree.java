18f694676b1a62bf740e75291aabaad406097af3
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-30 20:54:15 -0500

    Añadiendo metodos de rotacion simple izquierda y derecha en AVLTree

--- Laboratorios/Laboratorio7/Ejercicio2/src/Ejercicio/AVLtree.java
+++ Laboratorios/Laboratorio7/Ejercicio2/src/Ejercicio/AVLtree.java
@@ -18,4 +18,22 @@ public class AVLTree<T extends Comparable<T>> {
     private int balanceFactor(AVLNode<T> nodo) {
         return nodo == null ? 0 : altura(nodo.izquierda) - altura(nodo.derecha);
     }
+    private AVLNode<T> rotacionSimpleIzquierda(AVLNode<T> z) {
+        AVLNode<T> y = z.derecha;
+        AVLNode<T> T2 = y.izquierda;
+        y.izquierda = z;
+        z.derecha = T2;
+        z.altura = Math.max(altura(z.izquierda), altura(z.derecha)) + 1;
+        y.altura = Math.max(altura(y.izquierda), altura(y.derecha)) + 1;
+        return y;
+    }
+    private AVLNode<T> rotacionSimpleDerecha(AVLNode<T> z) {
+        AVLNode<T> y = z.izquierda;
+        AVLNode<T> T3 = y.derecha;
+        y.derecha = z;
+        z.izquierda = T3;
+        z.altura = Math.max(altura(z.izquierda), altura(z.derecha)) + 1;
+        y.altura = Math.max(altura(y.izquierda), altura(y.derecha)) + 1;
+        return y;
+    }
 }

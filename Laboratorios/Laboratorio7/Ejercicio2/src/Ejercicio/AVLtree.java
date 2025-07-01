b6e95336e858cb77858b406279010b33ae2a12c0
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-30 20:47:39 -0500

    Clase AVlTree constructor y atributos

---
+++ Laboratorios/Laboratorio7/Ejercicio2/src/Ejercicio/AVLtree.java
@@ -0,0 +1,10 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase arbol AVL
+public class AVLTree<T extends Comparable<T>> {
+    private AVLNode<T> raiz;
+    public AVLTree (){
+        raiz=null;
+    }
+
+}

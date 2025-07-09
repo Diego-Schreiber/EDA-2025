26dbf31998dee5a6335f34a23f98aed5efe37582
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-09 15:57:42 -0500

    Metodos de insercion

--- Laboratorios/Laboratorio8/Ejercicio7/src/Ejercicio/BPlusTree.java
+++ Laboratorios/Laboratorio8/Ejercicio7/src/Ejercicio/BPlusTree.java
@@ -15,4 +15,30 @@ public class BPlusTree<T extends Comparable<T>> {
     public boolean isEmpty() {
         return root.keys.isEmpty();
     }
+    public void insert(T key) {
+        BPlusNode<T> r = root;
+        if (r.keys.size() == 2 * order - 1) {
+            BPlusNode<T> s = new BPlusNode<>(false);
+            s.children.add(r);
+            dividedNode(s, 0);
+            root = s;
+        }
+        insertNonFull(root, key);
+    }
+    private void insertNonFull(BPlusNode<T> node, T key) {
+        int i = Collections.binarySearch(node.keys, key);
+        if (i >= 0) return;
+        i = -(i + 1);
+        if (node.isLeaf) {
+            node.keys.add(i, key);
+        } else {
+            BPlusNode<T> child = node.children.get(i);
+            if (child.keys.size() == 2 * order - 1) {
+                dividedNode(node, i);
+                if (key.compareTo(node.keys.get(i)) > 0)
+                    i++;
+            }
+            insertNonFull(node.children.get(i), key);
+        }
+    }
 }

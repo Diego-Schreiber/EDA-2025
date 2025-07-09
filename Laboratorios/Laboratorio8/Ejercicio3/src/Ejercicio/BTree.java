5c5bd6282a383a9ebf0807739b1a2d61451432d7
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-09 15:08:51 -0500

    Añadiendo metodos para insercion

--- Laboratorios/Laboratorio8/Ejercicio3/src/Ejercicio/BTree.java
+++ Laboratorios/Laboratorio8/Ejercicio3/src/Ejercicio/BTree.java
@@ -15,4 +15,55 @@ class BTree <Textends Comparable<T>>{
     public boolean isEmpty() {
         return root == null || root.keys.isEmpty();
     }
+    public void insert(T key) {
+        if (root.keys.size() == 2 * t - 1) {
+            Node<T> newRoot = new Node<>(false);
+            newRoot.children.add(root);
+            root = newRoot;
+            split(root, 0);
+        }
+        insertNonFull(root, key);
+    }
+    private void insertNonFull(Node<T> node, T key) {
+        int i = node.keys.size() - 1;
+        if (node.isLeaf) {
+            node.keys.add(null);
+            while (i >= 0 && key.compareTo(node.keys.get(i)) < 0) {
+                node.keys.set(i + 1, node.keys.get(i));
+                i--;
+            }
+            node.keys.set(i + 1, key);
+        } else {
+            while (i >= 0 && key.compareTo(node.keys.get(i)) < 0) {
+                i--;
+            }
+            i++;
+
+            Node<T> child = node.children.get(i);
+            if (child.keys.size() == 2 * t - 1) {
+                split(node, i);
+                if (key.compareTo(node.keys.get(i)) > 0) {
+                    i++;
+                }
+            }
+            insertNonFull(node.children.get(i), key);
+        }
+    }
+    private void split(Node<T> parent, int index) {
+        Node<T> fullNode = parent.children.get(index);
+        Node<T> newNode = new Node<>(fullNode.isLeaf);
+        T midKey = fullNode.keys.get(t - 1);
+        List<T> keysToMove = new ArrayList<>(fullNode.keys.subList(t, fullNode.keys.size()));
+        newNode.keys.addAll(keysToMove);
+        fullNode.keys.subList(t - 1, fullNode.keys.size()).clear();
+        if (!fullNode.isLeaf) {
+            List<Node<T>> childrenToMove = fullNode.children.subList(t, fullNode.children.size());
+            for (Node<T> child : childrenToMove) {
+                newNode.children.add(child);
+            }
+            childrenToMove.clear();
+        }
+        parent.keys.add(index, midKey);
+        parent.children.add(index + 1, newNode);
+    }
 }

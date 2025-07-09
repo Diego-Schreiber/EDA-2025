abe736a2058433c5f491e94ee8912d20a2624e14
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-09 14:57:22 -0500

    Iniciando clase BTree construcot ry atributos

---
+++ Laboratorios/Laboratorio8/Ejercicio3/src/Ejercicio/BTree.java
@@ -0,0 +1,12 @@
+package Ejercicio;
+/7Autor: Diego Schreiber
+//Clase Arbol B
+import java.util.*;
+class BTree <Textends Comparable<T>>{
+    private int t;
+    private Node<T> root;
+    public BTree(int t) {
+        this.t = t;
+        this.root = new Node<>(true);
+    }
+}

3647088b0fda8115ca9005bf0b38cf6f55fa5425
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-13 23:42:21 -0500

    Clase register atributos y constructor

---
+++ Laboratorios/Laboratorio9/Ejercicio1/src/Ejercicio/Register.java
@@ -0,0 +1,13 @@
+package Ejercicio;
+//Autor: Diego Schreiber
+//Clase register
+public Register<E> implements Comparable <Register<E>>{
+    private int key;
+    private E value;
+    private boolean deleted;
+    public Register(int key, E value){
+        this.key=key;
+        this.value=value;
+        this.deleted=false;
+    }
+}

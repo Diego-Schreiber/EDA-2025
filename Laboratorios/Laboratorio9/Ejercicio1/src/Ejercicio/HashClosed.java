3b7d05d2f41d89abc0117fa51112f2b54c02ae5f
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-13 23:55:56 -0500

    Correcion

--- Laboratorios/Laboratorio9/Ejercicio1/src/Ejercicio/HashClosed.java
+++ Laboratorios/Laboratorio9/Ejercicio1/src/Ejercicio/HashClosed.java
@@ -1,15 +1,15 @@
 package Ejercicio;
 //Autor: Diego Schreiber
-//Clase de hash cerrado
-public class HashClosed<E>{
+//Clase hash cerrado
+public class HashClosed<E> {
     private Register<E>[] table;
     private int size;
-    @SupressWarnings("unchecked")
-    public HashClosed(int capacity){
-        table=new Register[capacity];
-        size=0;
+    @SuppressWarnings("unchecked")
+    public HashClosed(int capacity) {
+        table = new Register[capacity];
+        size = 0;
     }
-    public HashClosed(){
+    public HashClosed() {
         this(16);
     }
     private int hash(int key) {
